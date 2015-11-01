28850fb5b2a9781f9adeaa6f3e40478e026c00ff
achim <astrohm@gmx.net>
2015-11-01 21:04:35 +0100

    startbutton bei 2 spielern aktiv, laufschrift läuft

--- WebContent/assets/js/changeStyle.js
+++ WebContent/assets/js/changeStyle.js
@@ -1,9 +1,13 @@
 
 document.addEventListener("DOMContentLoaded", init, false); //Event wird geworfen, wenn DOM-Baum geladen
+var playerCount = 0;
+var headline = "WebQuiz";
+var begin = 0;
+var end = headline.length;
 
 function init(){
 	//alert("init aufgerufen");
-	
+	lauftext();
 	var buttonStyleStandard = document.getElementById("buttonStyleStandard").addEventListener("click", changeStyleStandard, false);
 	var buttonStyleChristmas = document.getElementById("buttonStyleChristmas");
 	var buttonStyleHalloween = document.getElementById("buttonStyleHalloween");
@@ -23,6 +27,22 @@ function init(){
 	catalogSysProg.addEventListener("click", changeBackgroundColorOnClick, false);
 	catalogSysProg.parameter = "sysprog";
 	
+	var startButton = document.getElementById("buttonStart").disabled = true; //Start Button deaktivieren
+	//startButton.style = "background-color: black";
+	
+}
+
+
+function lauftext(){
+	document.getElementById("headlineticker").value = "" +
+	headline.substring(begin,end) + " " + headline.substring(0,begin);
+	begin ++;
+	if(begin >= end)
+	{ 
+	 begin = 0; 
+	}
+	/* Laufgeschwindigkeit: Höhere Zahl = langsamer */
+	window.setTimeout("lauftext()", 300); 
 }
 
 
@@ -107,25 +127,39 @@ function changeBackgroundColorOnClick(event){
 
 
 function addPlayerToTable(){
+	
 	var playerName = document.getElementById("inputPlayerName").value; //Spielername aus Textfeld wird übergeben
 	var table = document.getElementById("tablePlayerlistBody");
+	var maxPlayer = 6;
 	//alert(document.getElementById("inputPlayerName"));
 	
-	if(playerName === ""){
-		alert("Bitte geben Sie einen Namen ein!");
+	
+	if(playerCount <= maxPlayer){
+		if(playerName === ""){ //wenn kein Name eingegebn und login geklickt wurde
+			alert("Bitte geben Sie einen Namen ein!");
+		}
+		else{
+			playerCount += 1;
+			alert(playerCount);
+			
+			var tr = document.createElement("tr");
+			var td1 = document.createElement("td");
+			var td2 = document.createElement("td");
+			var playerNameText = document.createTextNode(playerName);
+			
+			
+			td2.appendChild(playerNameText);
+			tr.appendChild(td1);
+			tr.appendChild(td2);
+			table.appendChild(tr);
+		}
+		
+		if(playerCount == 2){
+			var startButton = document.getElementById("buttonStart").disabled = false; //Start Button deaktivieren
+		}
 	}
 	else{
-		
-		var tr = document.createElement("tr");
-		var td1 = document.createElement("td");
-		var td2 = document.createElement("td");
-		var playerNameText = document.createTextNode(playerName);
-		
-		
-		td2.appendChild(playerNameText);
-		tr.appendChild(td1);
-		tr.appendChild(td2);
-		table.appendChild(tr);
+		alert("Maximale Spieleranzahl erreicht!");
 	}
 }
 
