fbc77bcce87c0a7f3f634dba5eb300e57329cf54
achim <astrohm@gmx.net>
2015-12-19 21:30:06 +0100

    added some code

--- src/SocketJSONMessage.java
+++ src/SocketJSONMessage.java
@@ -1,4 +1,87 @@
+import de.fhwgt.quiz.application.Player;
+import de.fhwgt.quiz.application.Quiz;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+
+
+
+
 
 public class SocketJSONMessage {
+	
+	private String JSONString; //enthält String, der aus JSON-Objekten mit der Methode toString() erzeugt wird
+	
+	private int messageType; //Typ der Nachricht, 1 = LoginRequest etc.
+	private Object[] messageData = new Object[6]; //String, z.B. Spielername, Fragen, Antworten, ...
+	
+	
+	/**
+	 * Konstruktor baut JSON-Objekt aus Empfangsdaten (JSONString)
+	 * @param JSONString String enthält Loginname, Katalogname
+	 * @throws JSONException
+	 */
+	public SocketJSONMessage(String JSONString) throws JSONException {
+		
+		this.JSONString = JSONString;
+		
+		//lege neues JSON.Objekt an
+		JSONObject jObject = new JSONObject(this.JSONString);
+		
+		//lese messageType aus
+		switch(jObject.getInt("messageType")){
+			
+			case 1: //LoginRequest
+				System.out.println("LoginRequest, SocketJSONMessage.java");
+				this.messageType = jObject.getInt("messageType");
+				this.messageData[0] = jObject.getString("loginName");
+				break;
+				
+				
+			case 5:
+				
+			case 7:
+				
+				
+			case 8:
+			case 10:
+				
+				
+				
+				
+				
+			default:
+					break;
+		
+		
+		
+		
+		}
+		
+		
+		
+		
+		
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Timer.java
+++ src/Timer.java
@@ -1,4 +1,72 @@
+import java.util.TimerTask;
+import org.json.JSONException;
 
-public class Timer {
+import de.fhwgt.quiz.error.QuizError;
+import de.fhwgt.quiz.application.Player;
+import de.fhwgt.quiz.application.Quiz;
+
+
+import javax.websocket.Session;
+
+
+
+
+/*
+ * Klasse für das Timout der Fragen
+ * Ist Timout einer Frage abgelaufen wird eine Nachricht
+ * mit den korrekten Antworten an den Client gesendet
+ * 
+ * Ein TimerTask ist eine Klasse, die uns Runnable implementieren lässt und 
+ * Operationen umfasst, die zu einem Zeitpunkt oder in einer beliebigen Wiederholung 
+ * ausgeführt werden sollen.
+ * http://openbook.rheinwerk-verlag.de/javainsel9/javainsel_14_007.htm 
+ */
+public class Timer extends TimerTask{
+
+	private Session tSession;
+	private Player tPlayer;
+	
+	
+	public Timer(Player tPlayer, Session tSession){
+		this.tPlayer = tPlayer;
+		this.tSession = tSession;
+	}
+	
+	
+	
+	@Override
+	public void run() {
+		System.out.println("Time out!");
+		
+	/*	try{
+			QuizError qError = new QuizError();
+			
+			try{
+				//erstelle Timeout-Nachricht und sende diese an den Client
+				//tSession.getBasicRemote().sendText(
+						// Parameter true -> Timeout abgelaufen + Index der richtigen Antwort
+						//new SocketJSONMessage(11, new Object[] { true, Quiz.getInstance().answerQuestion(tPlayer, -1, qError) } )
+				//		.getJSONString()
+					//	);
+			}
+		}*/
+		
+	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
